59ae10a9dc647caddb11defc5d829c7b5824ac64
Merlijn <MMJ.Warps@student.han.nl>
2019-04-26 10:43:12 +0200

    create tables begin

---
+++ database scripts/CREATE tables.sql
@@ -0,0 +1,10 @@
+// script om de tabellen te maken
+
+CREATE TABLE Gebruiker (
+gebruikersnaam VARCHAR(50) NOT NULL,
+voornaam VARCHAR(50) NOT NULL,
+achternaam VARCHAR(51) NOT NULL,
+adresregel1 VARCHAR(71) NOT NULL,
+
+);
+
