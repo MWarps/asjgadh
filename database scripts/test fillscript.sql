068960eb5386e0bd5adad17577758c69b3b0f346
Merlijn <MMJ.Warps@student.han.nl>
2019-04-26 11:19:57 +0200

    fillscript update

--- database scripts/test fillscript.sql
+++ database scripts/test fillscript.sql
@@ -1,2 +1,5 @@
 INSERT INTO Gebruiker
 VALUES ('MWarps', 'Merlijn', 'Warps', 'testadres', NULL, '1234 AB', 'Nijmegen', 'Nederland','20000101', 'merlijn@warps.nu', 'hashed', 5, 'test antwoord', 0);
+
+INSERT INTO Gebruiker
+VALUES ('test2', 'testvoornaam', 'testachternaam', 'testadres', NULL, '1234 AB', 'testplaats', 'testland','20000101', 'test@email.test', 'hashed', 5, 'test antwoord', 0);
