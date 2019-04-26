c2ff057846c2b61e2f4e06c458c9621b56d9d72b
Merlijn <MMJ.Warps@student.han.nl>
2019-04-26 10:52:00 +0200

    create tables update
    gebruikerstabel v1

--- database scripts/CREATE tables.sql
+++ database scripts/CREATE tables.sql
@@ -5,6 +5,17 @@ gebruikersnaam VARCHAR(50) NOT NULL,
 voornaam VARCHAR(50) NOT NULL,
 achternaam VARCHAR(51) NOT NULL,
 adresregel1 VARCHAR(71) NOT NULL,
+adresregel2 VARCHAR(71) NULL,
+postcode CHAR(7) NOT NUll,
+plaatsnaam VARCHAR(28) NOT NUll,
+land VARCHAR(9) NOT NULL,
+geboortedatum Date NOT NULL,
+email VARCHAR(254) NOT NULL,
+wachtwoord VARCHAR(28) NOT NULL,
+vraag Tinyint NOT NULL,
+antwoordtekst VARCHAR(50) NOT NULL,
+verkoper bit NOT NUll,
+CONSTRAINT PK_Gebruiker PRIMARY KEY (gebruikersnaam),
 
 );
 
