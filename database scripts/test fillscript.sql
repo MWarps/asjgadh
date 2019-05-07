f9c8a7c94065c0a2e8f41bae9684e857d9a7fb59
Roy <roygerrits7@gmail.com>
2019-05-07 11:07:13 +0200

    update test populatie
    geslacht toegevoegd

--- database scripts/test fillscript.sql
+++ database scripts/test fillscript.sql
@@ -1,3 +1,6 @@
+use iproject
+go 
+
 insert into dbo.vragen(vraagnr,vraag) 
     values (1,'Wat was de naam van uw eerste huisdier?'), 
            (2,'Welke kleur was uw eerste auto?'), 
@@ -9,7 +12,7 @@ insert into dbo.vragen(vraagnr,vraag)
 ; 
 
 INSERT INTO Gebruiker
-VALUES ('MWarps', 'Merlijn', 'Warps', 'testadres', NULL, '1234 AB', 'Nijmegen', 'Nederland','20000101', 'merlijn@warps.nu', 'hashed', 5, 'test antwoord', 0);
+VALUES ('MWarps', 'Merlijn', 'Warps', 'M','testadres', NULL, '1234 AB', 'Nijmegen', 'Nederland','20000101', 'merlijn@warps.nu', 'hashed', 5, 'test antwoord', 0);
 
 INSERT INTO Gebruiker
-VALUES ('test2', 'testvoornaam', 'testachternaam', 'testadres', NULL, '1234 AB', 'testplaats', 'testland','20000101', 'test@email.test', 'hashed', 5, 'test antwoord', 0);
+VALUES ('test2', 'testvoornaam', 'testachternaam', 'X','testadres', NULL, '1234 AB', 'testplaats', 'testland','20000101', 'test@email.test', 'hashed', 5, 'test antwoord', 0);
