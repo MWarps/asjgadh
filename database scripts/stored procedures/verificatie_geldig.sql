394d1348e7fdd381a6d7efa6f1843e91c939ee12
Merlijn <MMJ.Warps@student.han.nl>
2019-05-07 15:36:07 +0200

    script update

--- database scripts/stored procedures/verificatie_geldig.sql
+++ database scripts/stored procedures/verificatie_geldig.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE verificatie_geldig
 AS 
 BEGIN 
-DELETE FROM Gebruiker INNER JOIN Verificatie ON Gebruiker.gebruikersnaam = Verificatie.gebruikersnaam WHERE eindtijd > CURRENT_TIMESTAMP AND verifieerd = 0;
+DELETE w,e FROM Gebruiker w INNER JOIN Verificatie e ON Gebruiker.gebruikersnaam = Verificatie.gebruikersnaam WHERE eindtijd > CURRENT_TIMESTAMP AND verifieerd = 0;
 DELETE FROM Verificatie WHERE eindtijd > CURRENT_TIMESTAMP;
 END
