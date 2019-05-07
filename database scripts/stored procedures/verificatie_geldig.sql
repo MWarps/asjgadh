bf8128588da21e5cc310c0f1714f1398e99c22d5
Merlijn <MMJ.Warps@student.han.nl>
2019-05-07 15:33:21 +0200

    gebruiker check bij verificatie deleten

--- database scripts/stored procedures/verificatie_geldig.sql
+++ database scripts/stored procedures/verificatie_geldig.sql
@@ -1,5 +1,6 @@
 CREATE PROCEDURE verificatie_geldig
 AS 
 BEGIN 
-DELETE FROM Verificatie WHERE eindtijd > CURRENT_TIMESTAMP ;
+DELETE FROM Gebruiker INNER JOIN Verificatie ON Gebruiker.gebruikersnaam = Verificatie.gebruikersnaam WHERE eindtijd > CURRENT_TIMESTAMP AND verifieerd = 0;
+DELETE FROM Verificatie WHERE eindtijd > CURRENT_TIMESTAMP;
 END
