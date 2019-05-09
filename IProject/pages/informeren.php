7fcb8f0ca41b9efbf6464cf315c132f09a29e26f
Sem Hegeman <sem_hegeman@hotmail.com>
2019-05-09 10:42:42 +0200

    Informeren Sem

---
+++ IProject/pages/informeren.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p></p>
+            </div>
+            <div class="col-md-6">
+                <img>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <img>
+            </div>
+            <div class="col-md-6">
+                <p></p>
+            </div>
+        </div>
+    </div>
+<?php include 'includes/footer.php' ?>
