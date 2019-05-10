e21d3a17d937597beee98521bbcbce0fc0ee2cb8
Lucas Schaars <lucasschaars@hotmail.om>
2019-05-10 16:35:04 +0200

    mail 12

--- IProject/includes/functies.php
+++ IProject/includes/functies.php
@@ -248,7 +248,7 @@ function StuurRegistreerEmail($Email, $Code){
 
         $headers = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        $headers = "From:" .$from."/r/n";
+        $headers .= "From:" .$from;
 
         mail($to,$subject,$message, $headers);
 
