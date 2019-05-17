fa1c25a1338b2b42e7bcb1a6cc7314887633c337
Merlijn <MMJ.Warps@student.han.nl>
2019-05-17 15:47:22 +0200

    voorwerpen update

--- database scripts/3. Conversie script/ebay conversiescript.sql
+++ database scripts/3. Conversie script/ebay conversiescript.sql
@@ -27,22 +27,24 @@ FROM ebay.dbo.Categorieen
 
 go 
 
-insert into dbo.Voorwerp (voorwerpnr, titel, beschrijving, startprijs, betalingswijze, plaatsnaam, Land, looptijd, looptijdbegindagtijdstip, verkoper, looptijdeindedagtijdstip, veilinggesloten)
-select distinct
-	ID as voorwerpnr,
-	Titel as titel,
-	Beschrijving as beschrijving,
-	Prijs as startprijs,
-	'Paypal' as betalingswijze,
-	Locatie as plaatsnaam,
-	select code from dbo.Landen where  as land,
-	7 as looptijd,
-	GETDATE() as looptijdbegindagtijdstip,
-	Verkoper as verkoper,
-	DATEADD(day, 7, CURRENT_TIMESTAMP) as looptijdeindedagtijdstip, -- Moet nog aangepast worden zodat alle items niet tegelijk worden verwijderd
-	0 as veilinggesloten
-	from dbo.Items
+INSERT INTO EenmaalAndermaal.dbo.Voorwerp (voorwerpnr, titel, beschrijving, startprijs, betalingswijze, plaatsnaam, Land, looptijd, looptijdbegindagtijdstip, verkoper, looptijdeindedagtijdstip, veilinggesloten)
+SELECT DISTINCT
+	ID AS voorwerpnr,
+	Titel AS titel,
+	Beschrijving AS beschrijving,
+	Prijs AS startprijs,
+	'Paypal' AS betalingswijze,
+	Locatie AS plaatsnaam,
+	(SELECT EenmaalAndermaal.dbo.Landen.GBA_CODE FROM EenmaalAndermaal.dbo.Landen WHERE EenmaalAndermaal.dbo.Landen LIKE CONCAT('%', Locatie, '%')) AS land,
+	7 AS looptijd,
+	GETDATE() AS looptijdbegindagtijdstip,
+	Verkoper AS verkoper,
+	--DATEADD(day, 7, CURRENT_TIMESTAMP) AS looptijdeindedagtijdstip, -- Moet nog aangepast worden zodat alle items niet tegelijk worden verwijderd
+	DATEADD(second, CAST(RAND() * 10 AS INT), DATEADD(day, 7, CURRENT_TIMESTAMP)) AS looptijdeindedagtijdstip, -- CAST(RAND() * 10 AS INT)
+	0 AS veilinggesloten
+	FROM ebay.dbo.Items
 
 	UPDATE Voorwerp
 	SET plaatsnaam = 'Nederland'
 	WHERE plaatsnaam like '%,Nederland%'
+	
