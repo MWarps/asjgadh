648f215895a0cf7150ba610e60a0cf2d5a8077d7
Merlijn <MMJ.Warps@student.han.nl>
2019-05-21 11:40:06 +0200

    query update

--- database scripts/query's/voorwerpen_uit_rubriek.sql
+++ database scripts/query's/voorwerpen_uit_rubriek.sql
@@ -1,4 +1,4 @@
-DECLARE @parent INT = 0; -- vul op de plaats van de nul de superrubriek waar je de items uit wil in
+DECLARE @parent INT = 1; -- vul op de plaats van de nul de superrubriek waar je de items uit wil in
 WITH cte AS
 (
 select null superrubriek, @parent rubrieknummer
@@ -7,8 +7,8 @@ SELECT  a.superrubriek, a.rubrieknummer
 	FROM dbo.Rubrieken a
 	WHERE a.superrubriek = @parent
 	UNION ALL
-SELECT a.superrubriek, a.rubrieknummer
-	FROM dbo.Rubrieken a JOIN cte c ON a.superrubriek = c.rubrieknummer
+SELECT b.superrubriek, b.rubrieknummer
+	FROM dbo.Rubrieken b JOIN cte c ON b.superrubriek = c.rubrieknummer
 												)
 
 SELECT TOP 20 * --hier aangeven welke waardes gereturned worden
