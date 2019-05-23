9912f75cb6edc17a4073dc6e6f53cea63b1756b4
Merlijn <MMJ.Warps@student.han.nl>
2019-05-23 15:18:59 +0200

    catalogus producten update

--- IProject/includes/functies.php
+++ IProject/includes/functies.php
@@ -199,40 +199,26 @@ function DetailAdvertentie($id)
 /* advertentie ophalen */
 function haalAdvertentieOp($rubriek){
     try {
-        require('core/dbconnection.php');
-        $sqlSelect = $dbh->prepare("select top 20 *, illustratieFile from Voorwerp, Voorwerpinrubriek, Illustratie
-        where Voorwerp.voorwerpnr = Voorwerpinrubriek.voorwerpnr 
-        AND Voorwerp.voorwerpnr = Illustratie.voorwerpnr
-		    AND Voorwerpinrubriek.rubrieknr = 157347
-        AND illustratiefile like 'dt_1%' ");
+        $producten = getProductenUitRubriek($rubriek, 20);
 
-        $sqlSelect->execute(
-        /* array(
-              ':rubriek' => $rubriek                 
-          )*/
-        );
-              $row = $sqlSelect->fetchAll(PDO::FETCH_ASSOC);
-              //  print_r($row);
-              $teller = 0;
-        foreach ($row as $rij => $id) {
-          if(strlen($row[$teller]['titel']) >= 40){
-            $row[$teller]['titel'] = substr($row[$teller]['titel'],0,40);
-            $row[$teller]['titel'] .= '...';
+        foreach ($producten as $rij => $id) {
+          if(strlen($rij['titel']) >= 40){
+              $rij['titel'] = substr($rij['titel'],0,40);
+              $rij['titel'] .= '...';
           }
             echo '
             <div class="col-md-4 pb-3">
             <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="../pics/'.$row[$teller]['illustratieFile'].'" alt="Foto bestaat niet">
-            <h5 class="card-header"><a href="advertentie.php?id='.$row[$teller]['voorwerpnr'].'">'.$row[$teller]['titel'].'</a></h5>
+            <img class="card-img-top" src="../pics/'.$rij['illustratieFile'].'" alt="Foto bestaat niet">
+            <h5 class="card-header"><a href="advertentie.php?id='.$rij['voorwerpnr'].'">'.$rij['titel'].'</a></h5>
             <div class="card-body">
-            <h4 class="card-text">'.$row[$teller]['startprijs'].'</h4>
-            <p class="card-text"><a href="#">'.$row[$teller]['verkoper'].'</a><br>
-            '.$row[$teller]['land'].', '.$row[$teller]['plaatsnaam'].'</p>
-            <a href="advertentie.php?id='.$row[$teller]['voorwerpnr'].'" class="btn btn-block btn-primary">Ga naar artikel</a>
+            <h4 class="card-text">'.$rij['startprijs'].'</h4>
+            <p class="card-text"><a href="#">'.$rij['verkoper'].'</a><br>
+            '.$rij['land'].', '.$rij['plaatsnaam'].'</p>
+            <a href="advertentie.php?id='.$rij['voorwerpnr'].'" class="btn btn-block btn-primary">Ga naar artikel</a>
             </div>
             </div>
             </div>';
-            $teller++;
         }
     
     } catch (PDOexception $e) {
