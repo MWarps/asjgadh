bc16a63e551971ee5278c379bb0c072ed7f1c678
Sem Hegeman <sem_hegeman@hotmail.com>
2019-05-27 14:24:38 +0200

    Update

--- IProject/index.php
+++ IProject/index.php
@@ -79,18 +79,17 @@ if(isset($_SESSION['status'])){
             <div class="hovereffect">
                 <img class="img-responsive" src="<?php echo $advertentie['illustratieFile']?>" alt="Geen afbeelding beschikbaar">
                 <div class="overlay">
-                    <h2><?= $advertentie['titel']?></h2>
+                    <h2><?php echo $advertentie['titel'];?></h2>
                     <a class="info" href=\"advertentie.php?id=<?php echo $advertentie['voorwerpnr']; ?>"><?php echo $advertentie['titel'];?></a>
                 </div>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-            <?php $advertentie = DetailAdvertentie($laatstBekeken[1]); 
-           ?>
+            <?php $advertentie = DetailAdvertentie($laatstBekeken[1]);?>
             <div class="hovereffect">
                 <img class="img-responsive" src="<?php echo $advertentie['illustratieFile'];?>" alt="Geen afbeelding beschikbaar">
                 <div class="overlay">
-                    <h2><?= $advertentie['titel']?></h2>
+                    <h2><?php echo $advertentie['titel'];?></h2>
                     <a class="info" href="advertentie.php?id=<?php echo $advertentie['voorwerpnr']; ?>"><?php echo $advertentie['titel'];?></a>
                 </div>
             </div>
@@ -101,7 +100,7 @@ if(isset($_SESSION['status'])){
             <div class="hovereffect">
                 <img class="img-responsive" src="<?php echo $advertentie['illustratieFile'];?>" alt="Geen afbeelding beschikbaar">
                 <div class="overlay">
-                    <h2><?= $advertentie['titel']?></h2>
+                    <h2><?php echo $advertentie['titel'];?></h2>
                     <a class="info" href="advertentie.php?id=<?php echo $advertentie['voorwerpnr']; ?>"><?php echo $advertentie['titel'];?></a>
                 </div>
             </div>
