663210dc37870932bb0574f99af0071ee3d2fc1c
Roy <roygerrits7@gmail.com>
2019-06-03 16:05:04 +0200

    cronjob voor bot advertenties

---
+++ IProject/core/cronJob.php
@@ -0,0 +1,30 @@
+<?php
+require 'includes/functies.php';
+$klantteller = 0;
+$botteller = 0;
+try {
+    require ('dbconnection.php');
+    $sluitVeiling = $dbh -> prepare(' update Voorwerp set veilinggesloten = 1  where voorwerpnr = :voorwerpnr ');
+   
+
+    $haalVeilingenOp = $dbh -> prepare(' select * from Voorwerp V, Gebruiker G where  G.gebruikersnaam = V.verkoper and G.email not like '%asjgadh%' ');
+    $botVeiling $dbh -> prepare(' select * from Voorwerp V, Gebruiker G where  G.gebruikersnaam = V.verkoper and G.email  like 'asjgadh%' ');
+
+    $haalVeilingenOp -> execute();
+    $botVeiling -> execute();
+
+    $klanten =  $haalVeilingenOp = $sqlSelect->fetch(PDO::FETCH_ASSOC);
+    $bots =  $botVeiling = $sqlSelect->fetch(PDO::FETCH_ASSOC);
+
+    foreach ($bots as bot){
+         $sluitVeiling -> execute( array(':voorwerpnr' => $bot[teller]['voorwerpnr'] ));
+        $teller ++; // moet laatste regel zijn.
+    }
+    
+    
+
+
+} catch {
+
+}
+?>
