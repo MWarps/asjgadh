b7b5ecb436c12d4e5f6e3a29d4a9c4c0bf8dbef8
Roy <roygerrits7@gmail.com>
2019-06-03 16:45:37 +0200

    cronjob voor eindedag

--- IProject/core/cronJob.php
+++ IProject/core/cronJob.php
@@ -1,32 +1,33 @@
 <?php
-require 'includes/functies.php';
+echo ('php runt');
+require '../includes/functies.php';
 $klantteller = 0;
 $botteller = 0;
 try {
     require ('dbconnection.php');
     $sluitVeiling = $dbh -> prepare(' update Voorwerp set veilinggesloten = 1  where voorwerpnr = :voorwerpnr ');
 
+    $haalVeilingenOp = $dbh -> prepare(' select * from Voorwerp V, Gebruiker G where  G.gebruikersnaam = V.verkoper and G.email not like %asjgadh% ');
+    $botVeiling = $dbh -> prepare(' select * from Voorwerp V, Gebruiker G where  G.gebruikersnaam = V.verkoper and G.email  like asjgadh% ');
 
-    $haalVeilingenOp = $dbh -> prepare(' select * from Voorwerp V, Gebruiker G where  G.gebruikersnaam = V.verkoper and G.email not like '%asjgadh%' ');
-    $botVeiling $dbh -> prepare(' select * from Voorwerp V, Gebruiker G where  G.gebruikersnaam = V.verkoper and G.email  like 'asjgadh%' ');
-
-    $haalVeilingenOp -> execute();
+    //$haalVeilingenOp -> execute();
     $botVeiling -> execute();
 
-    $klanten =  $haalVeilingenOp = $sqlSelect->fetch(PDO::FETCH_ASSOC);
-    $bots =  $botVeiling = $sqlSelect->fetch(PDO::FETCH_ASSOC);
+    //$klanten =  $haalVeilingenOp = $sqlSelect->fetch(PDO::FETCH_ASSOC);
+    $bots = $botVeiling ->fetch(PDO::FETCH_ASSOC);
 
     foreach ($bots as bot){
-        if (date("d.m.Y H:i", strtotime($bot[teller]['looptijdeindedagtijdstip']))=< date("d.m.Y H:i"){
+        echo('foreach active');
+        if ((date("d.m.Y H:i", strtotime($bot[teller]['looptijdeindedagtijdstip'])) ) =< date("d.m.Y H:i")){
+                echo ('timecheck if-statement active');
+                $sluitVeiling -> execute( array(':voorwerpnr' => $bot[teller]['voorwerpnr'] ));
+                $teller ++; // moet laatste regel zijn.
+            }else{
+                echo ('timecheck if-statement ELSE active');
+            }
+            }
 
-            $sluitVeiling -> execute( array(':voorwerpnr' => $bot[teller]['voorwerpnr'] ));
-            $teller ++; // moet laatste regel zijn.
-        }
-    }
+            } catch {
 
-
-
-} catch {
-
-}
+            }
 ?>
