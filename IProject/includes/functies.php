4eea8b8c1fa14189e88d31f0ae4cde36313942f6
Sandra ter Maat <Sandra97termaat@hotmail.nl>
2019-06-04 11:21:04 +0200

    Werkt dit? :D

--- IProject/includes/functies.php
+++ IProject/includes/functies.php
@@ -1572,8 +1572,9 @@ function veilingblok($voorwerpnummer){
             );
             veilingeindberekenen ($resultaat[0]['voorwerpnr']);
         }else if ($resultaat[0]['geblokkeerd'] == 0){
-           // $records =  HaalBiederEnVerkoperOp($voorwerpnummer, $verkoper);
-           // VerstuurVeilingBlockedMail($veiling, $ontvanger;
+            $Veiling = HaalBiederEnVerkoperOp($_GET['id'], $_SESSION['gebruikersnaam']);
+            VerstuurVeilingBlockedMail($Veiling, true);
+            VerstuurVeilingBlockedMail($Veiling, false);
             $blokeren -> execute(
                 array(
                     ':voorwerpnummer' => $resultaat[0]['voorwerpnr'],
